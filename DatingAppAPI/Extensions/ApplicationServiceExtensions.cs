5e628e809f5b62a21f2ad898024dffa8bcb1c047
RAJUL JADIA <Rajuljadia31@outlook.com>
2022-06-09 23:31:38 +0530

    switch to postgres

--- DatingAppAPI/Extensions/ApplicationServiceExtensions.cs
+++ DatingAppAPI/Extensions/ApplicationServiceExtensions.cs
@@ -26,7 +26,27 @@ namespace DatingAppAPI.Extensions
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                string connStr;
+                if(env == "Development")
+                {
+                    connStr = config.GetConnectionString("DefaultConnection");
+                }else
+                {
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                }
+                options.UseNpgsql(connStr);
             });
             return services;
         }
