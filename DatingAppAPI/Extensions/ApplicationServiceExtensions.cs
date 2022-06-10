1ca6efe9672458523e2e8852d2adc1c686976825
RAJUL JADIA <Rajuljadia31@outlook.com>
2022-06-10 12:46:14 +0530

    change to require

--- DatingAppAPI/Extensions/ApplicationServiceExtensions.cs
+++ DatingAppAPI/Extensions/ApplicationServiceExtensions.cs
@@ -43,7 +43,7 @@ namespace DatingAppAPI.Extensions
                     var pgPass = pgUserPass.Split(":")[1];
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
-                    var pgSSL = "Prefer";
+                    var pgSSL = "Require";
 
                     connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode={pgSSL};";
                 }
